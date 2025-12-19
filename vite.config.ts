033abb8bc86ccae9f413deed1ff72eeb39d3e3f8
quanphung1120 <phungthequan030@gmail.com>
2025-12-20 03:41:02 +0700

    refactor(build): use viteReact built-in babel config for react-compiler
    - Remove separate vite-plugin-babel dependency
    - Configure babel-plugin-react-compiler via viteReact's babel option
    - Remove redundant comment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
-import babel from 'vite-plugin-babel'
 
 const config = defineConfig({
   plugins: [
@@ -17,16 +16,13 @@ const config = defineConfig({
         },
       },
     }),
-
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
-    viteReact(),
-    babel({
-      babelConfig: {
+    viteReact({
+      babel: {
         plugins: ['babel-plugin-react-compiler'],
       },
     }),
